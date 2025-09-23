f527efdcd860f93e14f5026e7e77727a20de3208
Vafaure <valentin.faure2002@orange.fr>
2025-09-23 18:42:52 +0200

    add log-scale

--- Dashboard.py
+++ Dashboard.py
@@ -26,46 +26,51 @@ commodities = {"Crude Oil WTI": "CL=F","Crude Oil Brent": "BZ=F","Natural Gas": 
 horizon_map = {"1 Month": "1mo","3 Months": "3mo","6 Months": "6mo","1 Year": "1y",
                "5 Years": "5y","10 Years": "10y","20 Years": "20y","Max": "max"}
 
+
 with col1:
     with st.container(border=True):
+        st.markdown("#### Parameters")
         commodities_name = st.multiselect("Commodities", list(commodities.keys()))
         stock_names = st.multiselect("Stock", list(companies.keys()), default=["Apple"])
+        horizon = st.pills("Horizon", list(horizon_map.keys()), default="1 Month")
+        log_scale = st.toggle("Log scale", value=False)
     
 
-with col2:
-    with st.container(border=True):
-        horizon = st.pills(None, list(horizon_map.keys()), default="1 Month")
-        tickers = [companies[name] for name in stock_names]
-        data = yf.download(tickers, period=horizon_map[horizon])["Close"]
 
-        # création du graphique
-        if len(tickers) == 1:
-            fig = px.line(data, x=data.index, y=data.columns,title=None)
+tickers = [companies[name] for name in stock_names] + [commodities[name] for name in commodities_name]
+data = yf.download(tickers, period=horizon_map[horizon])["Close"]
+
+
+
+with col2:
+    if len(tickers) == 1:
+        fig = px.line(data, x=data.index, y=data.columns,title=None)
+    else:
+        data_norm = (data.pct_change().fillna(0) + 1).cumprod()
+        if log_scale == True:
+            fig = px.line(data_norm, x=data_norm.index, y=data_norm.columns, title="Normalized performances")
+            fig.update_yaxes(type="log")
         else:
-            data_norm = (data.pct_change().fillna(0) + 1).cumprod()
             data_percent = (data_norm-1)*100
             fig = px.line(data_percent, x=data_percent.index, y=data_percent.columns, title="Normalized performances")
             fig.update_yaxes(ticksuffix="%")
 
-        st.plotly_chart(fig)
+    st.plotly_chart(fig)
 
-
-with col1:
-    with st.container(border=True):
-        if len(tickers)==1:
-            start_price = data.iloc[0] 
-            end_price = data.iloc[-1] 
-            évolution = ((end_price-start_price)/start_price)*100
-            st.metric("Growth", value=f"${end_price.item():,.2f}", delta=f"{évolution.item():,.2f}%")
-        else:
-            perf = (data.iloc[-1] / data.iloc[0] - 1) * 100
-            best = perf.idxmax()
-            worst = perf.idxmin()
-            col_best, col_worst = st.columns(2)
-            with col_best:
-                st.metric("Best growth",value=f"{best}",delta=f"{perf[best]:,.2f}%")
-            with col_worst:
-                st.metric("Worst growth", value=f"{worst}",delta=f"{perf[worst]:,.2f}%")
+    if len(tickers)==1:
+        start_price = data.iloc[0] 
+        end_price = data.iloc[-1] 
+        évolution = ((end_price-start_price)/start_price)*100
+        st.metric("Growth", value=f"${end_price.item():,.2f}", delta=f"{évolution.item():,.2f}%")
+    else:
+        perf = (data.iloc[-1] / data.iloc[0] - 1) * 100
+        best = perf.idxmax()
+        worst = perf.idxmin()
+        col_best, col_worst = st.columns(2)
+        with col_best:
+            st.metric("Best growth",value=f"{best}",delta=f"{perf[best]:,.2f}%")
+        with col_worst:
+            st.metric("Worst growth", value=f"{worst}",delta=f"{perf[worst]:,.2f}%")
                 
        
 st.markdown("### Raw data")
